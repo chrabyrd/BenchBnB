36b0ecb55b41f1c7899b78c46f535248a0b064b0
Christopher <chrabyrd@gmail.com>
2017-01-09 14:33:11 -0800

    Set up backend user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,31 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def log_in(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out_user
+    @current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  private
+
+  def require_sign_in
+    render json: { base: ['invalid credentials'] }, status: 401 unless logged_in?
+  end
 end
