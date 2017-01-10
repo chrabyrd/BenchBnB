929708690865135255c95be4496485cd589ed133
Christopher <chrabyrd@gmail.com>
2017-01-09 16:47:54 -0800

    Finish react router

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 import configureStore from './store/store';
-// import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
-  // window.signup = signup;
-  // window.login = login;
-  // window.logout = logout;
-  window.store = configureStore();
+  ReactDOM.render(<Root store={ store }/>, root);
 });
